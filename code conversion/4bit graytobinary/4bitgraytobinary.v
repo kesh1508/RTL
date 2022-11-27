924614833ed01088f07fb61945bf36cfb8c99d32
Opportunities are closer than you think <48319804+kesh1508@users.noreply.github.com>
2022-11-27 20:33:31 +0530

    Add files via upload

---
+++ code conversion/4bit graytobinary/4bitgraytobinary.v
@@ -0,0 +1,8 @@
+module 4bit_gray_to_binary (G, B);
+input [3:0]G;
+output [3:0]B;
+assign B[3] = G[3];
+assign B[2] = G[3] ^ G[2];
+assign B[1] = G[3] ^ G[2] ^ G[1];
+assign B[0] = G[3] ^ G[2] ^ G[1] ^ G[0];
+endmodule
